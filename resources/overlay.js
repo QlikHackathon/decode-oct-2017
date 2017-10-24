ef67c521e53a2c80e52a1ff76a28f136fbb6aaaf
Jesse Okeya <Jesseokeya@gmail.com>
2017-10-24 16:36:29 -0400

    Trying To Make Scroll Obvious

--- resources/overlay.js
+++ resources/overlay.js
@@ -1,9 +1,14 @@
 function openOverlay() {
     document.getElementById("qOverlay").style.animationName = "open-overlay";
+    $('#qOverlay').show();
 }
 
 function closeOverlay() {
     document.getElementById("qOverlay").style.animationName = "close-overlay";
+    setTimeout(()=>{
+      $('#qOverlay').hide();
+    }, 1000);
+
 }
 
 $(window).bind('mousewheel', function(event) {
