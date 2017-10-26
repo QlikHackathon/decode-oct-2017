6a439799719b06db31cfc19939392fa2be2f6100
Rene-Jacques Riel <rie@qlik.com>
2017-10-26 18:34:52 -0400

    fixing lists

--- resources/script.js
+++ resources/script.js
@@ -10,41 +10,46 @@ var config = {
 
 function main() {
   require.config({
-    baseUrl: (config.isSecure ? 'https://' : 'http://') + config.host + (config.port ? ':' + config.port : '') + config.prefix + 'resources'
+    baseUrl:
+      (config.isSecure ? 'https://' : 'http://') +
+      config.host +
+      (config.port ? ':' + config.port : '') +
+      config.prefix +
+      'resources'
   })
 
   /**
    * Load the entry point for the Capabilities API family
    * See full documention: http://help.qlik.com/en-US/sense-developer/Subsystems/APIs/Content/MashupAPI/qlik-interface-interface.htm
    */
-  require(['js/qlik'], function (qlik) {
+  require(['js/qlik'], function(qlik) {
     // We're now connected
 
     // Suppress Qlik error dialogs and handle errors how you like.
-    qlik.setOnError(function (error) {
-      console.log("ERROR", error)
+    qlik.setOnError(function(error) {
+      console.log('ERROR', error)
     })
 
     // Open a dataset on the server
     app = qlik.openApp(config.appname, config)
-    let field = app.field("Goal ID")
-    field.selectValues(["Goal 14"], false, true)
+    let field = app.field('Goal ID')
+    field.selectValues(['Goal 14'], false, true)
     field.lock()
-    setupCountriesHyperCube();
-    setupOceansHyperCube();
-    setupFilterBarHyperCube();
+    setupCountriesHyperCube()
+    setupOceansHyperCube()
+    setupFilterBarHyperCube()
 
     createLeadEntityTypePieChart()
     createOceanBasinsPieChart()
     createLeadEntityPieChart()
     createTargetsPieChart()
 
-    var selState = app.selectionState( )
+    var selState = app.selectionState()
     var listener = function() {
       updateFilterBar(selState.selections)
     }
     //bind the listener
-    selState.OnData.bind( listener )
+    selState.OnData.bind(listener)
     createGoalsAndSDGTargets()
     createCommitmentList()
     createLeadEntityTypesList()
@@ -60,9 +65,11 @@ function createLeadEntityTypePieChart() {
     '=Count(distinct [Lead entity])'
   ]
 
-  app.visualization.create('treemap', listCols, { title: 'Lead Entity Type Pie Chart' }).then(function (piechart) {
-    piechart.show('lead-entity-type-pie-chart')
-  })
+  app.visualization
+    .create('treemap', listCols, { title: 'Lead Entity Type Pie Chart' })
+    .then(function(piechart) {
+      piechart.show('lead-entity-type-pie-chart')
+    })
 }
 
 function createLeadEntityPieChart() {
@@ -73,20 +80,24 @@ function createLeadEntityPieChart() {
     '=Count([OceanActionID])'
   ]
 
-  app.visualization.create('treemap', listCols, { title: 'Lead Entity Pie Chart' }).then(function (piechart) {
-    piechart.show('lead-entity-pie-chart')
-  })
+  app.visualization
+    .create('treemap', listCols, { title: 'Lead Entity Pie Chart' })
+    .then(function(piechart) {
+      piechart.show('lead-entity-pie-chart')
+    })
 }
 function createTargetsPieChart() {
   var listCols = [
     {
-      qDef: {qFieldDefs: ['SDG Target']}
+      qDef: { qFieldDefs: ['SDG Target'] }
     },
     '=Count([OceanActionID])'
   ]
-  app.visualization.create('treemap', listCols, {title: 'Targets Pie Charts'}).then(function (piechart) {
-    piechart.show('targets-pie-chart')
-  })
+  app.visualization
+    .create('treemap', listCols, { title: 'Targets Pie Charts' })
+    .then(function(piechart) {
+      piechart.show('targets-pie-chart')
+    })
 }
 
 function createOceanBasinsPieChart() {
@@ -97,9 +108,11 @@ function createOceanBasinsPieChart() {
     '=Count([OceanActionID])'
   ]
 
-  app.visualization.create('treemap', listCols, {title: 'Ocean Basins Pie Chart'}).then(function (piechart) {
-    piechart.show('ocean-basins-pie-chart')
-  })
+  app.visualization
+    .create('treemap', listCols, { title: 'Ocean Basins Pie Chart' })
+    .then(function(piechart) {
+      piechart.show('ocean-basins-pie-chart')
+    })
 }
 
 function createGoalsAndSDGTargets() {
@@ -107,17 +120,17 @@ function createGoalsAndSDGTargets() {
     qDimensions: [
       {
         qDef: {
-          qFieldDefs: ['Target Title'],
+          qFieldDefs: ['Target Title']
         }
       },
       {
         qDef: {
-          qFieldDefs:['Target Icon']
+          qFieldDefs: ['Target Icon']
         }
       },
       {
         qDef: {
-          qFieldDefs: ['Goal ID'],
+          qFieldDefs: ['Goal ID']
         }
       }
     ],
@@ -125,10 +138,9 @@ function createGoalsAndSDGTargets() {
       {
         qDef: { qDef: '=Count(OceanActionID) + (0 * Count({1}OceanActionID))' },
         qSortBy: { qSortByNumeric: -1 }
-      },
-
-   ],
-    qInterColumnSortOrder : [2, 0 ,1],
+      }
+    ],
+    qInterColumnSortOrder: [2, 0, 1],
     qInitialDataFetch: [
       {
         qTop: 0,
@@ -138,48 +150,49 @@ function createGoalsAndSDGTargets() {
       }
     ]
   }
-  app
-    .createCube(hyperCubeDef, hypercube => {
-      let matrix = hypercube.qHyperCube.qDataPages[0].qMatrix
-      var targets = document.getElementById("targets")
-      targets.innerHTML=""
-      matrix = matrix.filter((row) => row[2].qText === "Goal 14")
-      matrix.forEach((row, index) => {
-        var percentage = row[3].qNum / hypercube.qHyperCube.qGrandTotalRow[0].qNum;
-        percentage = Math.round(percentage * 100);
-        var target = $(`<div class="kpiElements"></div>`)
-        if(row[0].qState === "X") {
-          target.addClass("inactive")
-        }
-        target.append(`<h5>${row[0].qText}</h5>`);
-        target.append(`<img src="./resources/icons/${row[1].qText}.svg"></img>`);
-        target.append(`<h5>${row[3].qText}</h5>`);
-        target.append(`<h5>${percentage}%</h5>`);
-        target.click(function(){
-          let field = app.field("Target Title");
-          field.selectValues([row[0].qText], true, true)
-        })
-        $(`#targets`).append(target);
+  app.createCube(hyperCubeDef, hypercube => {
+    let matrix = hypercube.qHyperCube.qDataPages[0].qMatrix
+    var targets = document.getElementById('targets')
+    targets.innerHTML = ''
+    matrix = matrix.filter(row => row[2].qText === 'Goal 14')
+    matrix.forEach((row, index) => {
+      var percentage = row[3].qNum / hypercube.qHyperCube.qGrandTotalRow[0].qNum
+      percentage = Math.round(percentage * 100)
+      var target = $(`<div class="kpiElements"></div>`)
+      if (row[0].qState === 'X') {
+        target.addClass('inactive')
+      }
+      target.append(`<h5>${row[0].qText}</h5>`)
+      target.append(`<img src="./resources/icons/${row[1].qText}.svg"></img>`)
+      target.append(`<h5>${row[3].qText}</h5>`)
+      target.append(`<h5>${percentage}%</h5>`)
+      target.click(function() {
+        let field = app.field('Target Title')
+        field.selectValues([row[0].qText], true, true)
       })
+      $(`#targets`).append(target)
     })
+  })
 }
 
-function createGoalCountKpi (target) {
-  var listCols = ["=Count({<[SDG Target]={'"+target+"'}>}OceanActionID)"]
-  app.visualization.create('kpi', listCols, {
-    title: target + ' Commitments',
-    showTitles: true,
-    showMeasureTitle: false
-  }).then(function (kpi) {
-    kpi.show('goals-count-kpi-' + target)
-  })
+function createGoalCountKpi(target) {
+  var listCols = ["=Count({<[SDG Target]={'" + target + "'}>}OceanActionID)"]
+  app.visualization
+    .create('kpi', listCols, {
+      title: target + ' Commitments',
+      showTitles: true,
+      showMeasureTitle: false
+    })
+    .then(function(kpi) {
+      kpi.show('goals-count-kpi-' + target)
+    })
 }
 
 function clearState(state) {
   state = state || '$'
   app.clearAll(false, state)
   //except for Goal 14
-  app.field("Goal ID").selectValues(["Goal 14"], false, false)
+  app.field('Goal ID').selectValues(['Goal 14'], false, false)
 }
 
 function createCommitmentList() {
@@ -202,7 +215,7 @@ function createCommitmentList() {
       {
         qTop: 0,
         qLeft: 0,
-        qHeight: 100,
+        qHeight: 2000,
         qWidth: 2
       }
     ]
@@ -210,13 +223,13 @@ function createCommitmentList() {
 
   app.createCube(hyperCubeDef, hypercube => {
     let matrix = hypercube.qHyperCube.qDataPages[0].qMatrix
-    const $list = $('#commitmentList');
-    $list.empty();
+    const $list = $('#commitmentList')
+    $list.empty()
     matrix.forEach(row => {
       let anchor = $(
         `<a target='_blank' href='${row[1].qText}'>${row[0].qText}</a>`
       )
-      let item = $(`<div class='list-group-item'></div>`)
+      let item = $(`<div></div>`)
       item.append(anchor)
       $list.append(item)
     })
@@ -224,45 +237,69 @@ function createCommitmentList() {
 }
 
 function createLeadEntityTypesList() {
-
-    let field = app.field("Lead entity type")
-    let listener = () => {
-      const $list = $('#leadEntityTypeList');
-      $list.empty();
-      field.rows.forEach(row => {
-        let item = $(`<div class='list-group-item'>${row.qText}</div>`)
-        if(row.qState === "X") {
-          item.addClass("inactive")
+  const $list = $('#leadEntityTypeList')
+  app.createList(
+    {
+      qDef: {
+        qFieldDefs: ['Lead entity type'],
+        qSortCriterias: [{ qSortByState: 1 }, { qSortByAscii: 1 }]
+      },
+      qInitialDataFetch: [
+        {
+          qTop: 0,
+          qLeft: 0,
+          qHeight: 2000,
+          qWidth: 1
+        }
+      ]
+    },
+    function(reply) {
+      $list.empty()
+      reply.qListObject.qDataPages[0].qMatrix.forEach(row => {
+        let item = $(`<div>${row[0].qText}</div>`)
+        if (row[0].qState === 'X') {
+          item.addClass('inactive')
         }
         item.click(() => {
-          app.field('Lead entity type').selectValues([row.qText], true, true)
+          app.field('Lead entity type').selectValues([row[0].qText], true, true)
         })
         $list.append(item)
       })
+      $list.scrollTop(0)
     }
-
-    field.OnData.bind(listener)
-    field.getData()
-  }
+  )
+}
 
 function createLeadEntityList() {
-
-  let field = app.field("Lead entity")
-  let listener = () => {
-    const $list = $('#leadEntityList');
-    $list.empty();
-    field.rows.forEach(row => {
-      let item = $(`<div class='list-group-item'>${row.qText}</div>`)
-      if(row.qState === "X") {
-        item.addClass("inactive")
-      }
-      item.click(() => {
-        app.field('Lead entity').selectValues([row.qText], true, true)
+  const $list = $('#leadEntityList')
+  app.createList(
+    {
+      qDef: {
+        qFieldDefs: ['Lead entity'],
+        qSortCriterias: [{ qSortByState: 1 }, { qSortByAscii: 1 }]
+      },
+      qInitialDataFetch: [
+        {
+          qTop: 0,
+          qLeft: 0,
+          qHeight: 2000,
+          qWidth: 1
+        }
+      ]
+    },
+    function(reply) {
+      $list.empty()
+      reply.qListObject.qDataPages[0].qMatrix.forEach(row => {
+        let item = $(`<div>${row[0].qText}</div>`)
+        if (row[0].qState === 'X') {
+          item.addClass('inactive')
+        }
+        item.click(() => {
+          app.field('Lead entity').selectValues([row[0].qText], true, true)
+        })
+        $list.append(item)
       })
-      $list.append(item)
-    })
-  }
-
-  field.OnData.bind(listener)
-  field.getData()
+      $list.scrollTop(0)
+    }
+  )
 }
